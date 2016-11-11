d7282eae88797e78324b1f0deaf4feba56d7d83c
nicolas-min <nicolasmin@berkeley.edu>
2016-11-10 20:11:47 -0800

    iqr->interquartile_range
    thanks Ryan! looks awesome

--- project/code/scripts/create-team-salaries.R
+++ project/code/scripts/create-team-salaries.R
@@ -8,7 +8,7 @@ salary_aggregates = team_data %>% group_by(Team) %>% select(salary, Team) %>%
   summarise(total = sum(salary), minimum = min(salary), maximum = max(salary), 
             first_quartile = quantile(salary, .25), median = median(salary), 
             third_quartile = quantile(salary, .75), average = mean(salary),
-            iqr = IQR(salary), standard_deviation = sd(salary))
+            interquartile_range = IQR(salary), standard_deviation = sd(salary))
   
 
 write.csv(salary_aggregates, file = "data/cleandata/team-salaries.csv")
