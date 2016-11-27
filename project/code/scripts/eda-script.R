b70e8f850334872a8e80ecd6918709b29e4202a5
nghitrampham <nghitrampham@gmail.com>
2016-11-27 02:15:03 -0800

    updated eda

--- project/code/scripts/eda-script.R
+++ project/code/scripts/eda-script.R
@@ -5,7 +5,7 @@ setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
 # Source helper functions
 source("../functions/generate-statistics.R") #Brandon's
-#source("../functions/generate-plots.R") #Trams's
+source("../functions/generate-plots.R") #Trams's
 
 # List of fields to treat as pure text fields
 text_fields <- c("Player",
@@ -14,18 +14,22 @@ text_fields <- c("Player",
                  "Country",
                  "College")
 
-# Get the full player table and format Birth.Date column
+# Get the full player table
 t_location <- "../../data/cleandata/roster-salary-stats.csv"
 
 full_player_table <- read.csv(t_location,
-                       stringsAsFactors = FALSE)
+                              stringsAsFactors = FALSE)
 
-full_player_table$Birth.Date <- as.Date(full_player_table$Birth.Date)
+full_player_table$Birth.Date <-
+    as.Date(full_player_table$Birth.Date)
 
-# Generate EDA analysis data
 result <- create_summary_file(full_player_table, text_fields)
 if (!result) {
     print("Failure in generating summary statistic file.")
 }
 
-tram_function(full_player_table)
+# tram_function(full_player_table)
+data = full_player_table
+player_data = full_player_table
+bar_graphs <- create_plot_graphs(data, text_fields)
+hist_graphs <- create_box_histogram(player_data, text_fields)
